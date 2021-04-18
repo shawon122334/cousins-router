a48bdee885d0f132fb62d15839e8ada66f36f0fe
Sabbir Ahmed Shawon <shawon.mu11@gmail.com>
2021-04-19 02:32:30 +0600

    useHisory used

--- src/Components/Cousin/Cousin.js
+++ src/Components/Cousin/Cousin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Cousin = (props) => {
     const {name,email,id}= props.cousin;
@@ -9,12 +9,18 @@ const Cousin = (props) => {
         padding:'10px',
         borderRadius:'10px'
     }
+const history= useHistory();
+const handleClick=(id)=>{
+    const url=`/cousin/${id}`;
+    history.push(url);
+}
     return (
         <div style={cousinStyle}>
             <h2>name : {name}</h2>
             <p>email : {email}</p>
             <p>id : {id }</p>
-            <Link to={`/cousin/${id}`}>show details of id of {id}</Link>
+            <Link to={`/cousin/${id}`}><button>show details of id of {id}</button></Link>
+            <button onClick={()=>handleClick(id)}>Click Me</button>
         </div>
     );
 };
--- src/Components/ShowDetails/ShowDetails.js
+++ src/Components/ShowDetails/ShowDetails.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const ShowDetails = () => {
     const {cousinId}= useParams();
+    const [cousin,setCousin]= useState({});
+    useEffect(()=>{
+        const url=`https://jsonplaceholder.typicode.com/users/${cousinId}`;
+        fetch(url)
+        .then(response=>response.json())
+        .then(data=>setCousin(data))
+    },[])
+    const {name,email,phone,website}=cousin;
     return (
         <div>
             <h2>id is : {cousinId}</h2>
+            <h2>{name}</h2>
+            <h2>{email}</h2>
+            <h2>{phone}</h2>
+            <h2>{website}</h2>
         </div>
     );
 };
